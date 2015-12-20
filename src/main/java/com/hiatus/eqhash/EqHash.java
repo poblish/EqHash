fecaf88c1a7314c121f937322fe621810f01412f
Andrew Regan <aregan@gmail.com>
2015-12-20 12:37:35 +0000

    Fix indents

--- src/main/java/com/hiatus/eqhash/EqHash.java
+++ src/main/java/com/hiatus/eqhash/EqHash.java
@@ -15,43 +15,43 @@ import java.util.Set;
  */
 public final class EqHash {
 
-	private EqHash() {}
-	
-	/**
-	 * Test your equals() and hashCode() methods.
-	 * 
-	 * @param inObj an instance of your class
-	 * @param inCopy another instance that you assert is a copy of inObj
-	 * @param inDifferentObjects an array of objects you assert are all different from inObj
-	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void testEqualsHashcode( final Object inObj, final Object inCopy, final Object... inDifferentObjects)
-	{
-		// First up, check the types match...
-		assertThat((Class) inObj.getClass(), allOf( equalTo((Class) inCopy.getClass() ) ));
+    private EqHash() {}
 
-		assertThat( inObj.equals(null), is(false));  // NOPMD
-		assertThat( inObj.equals((Object) new CompletelyDifferentClass()), is(false));
-		assertThat( inObj, equalTo(inObj));
-		assertThat( "Copy object should not == source", inCopy, not( sameInstance(inObj) ));
-		assertThat( "Source object should equal the Copy object", inObj, equalTo(inCopy));
+    /**
+     * Test your equals() and hashCode() methods.
+     * 
+     * @param inObj an instance of your class
+     * @param inCopy another instance that you assert is a copy of inObj
+     * @param inDifferentObjects an array of objects you assert are all different from inObj
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static void testEqualsHashcode( final Object inObj, final Object inCopy, final Object... inDifferentObjects)
+    {
+        // First up, check the types match...
+        assertThat((Class) inObj.getClass(), allOf( equalTo((Class) inCopy.getClass() ) ));
 
-		assertThat( "Source.hashCode() should equal itself", inObj.hashCode(), equalTo( inObj.hashCode() ));
-		assertThat( "Source.hashCode() should equal the Copy.hashCode()", inObj.hashCode(), equalTo( inCopy.hashCode() ));
+        assertThat( inObj.equals(null), is(false));  // NOPMD
+        assertThat( inObj.equals((Object) new CompletelyDifferentClass()), is(false));
+        assertThat( inObj, equalTo(inObj));
+        assertThat( "Copy object should not == source", inCopy, not( sameInstance(inObj) ));
+        assertThat( "Source object should equal the Copy object", inObj, equalTo(inCopy));
 
-		final Set<Object> diffObjects = new HashSet<Object>();
-		diffObjects.addAll( Arrays.asList(inDifferentObjects) );
-		assertThat( "Duplicate Different objects are present: counts differ", inDifferentObjects.length, is(diffObjects.size()));
+        assertThat( "Source.hashCode() should equal itself", inObj.hashCode(), equalTo( inObj.hashCode() ));
+        assertThat( "Source.hashCode() should equal the Copy.hashCode()", inObj.hashCode(), equalTo( inCopy.hashCode() ));
 
-		for ( Object eachDiffObject : inDifferentObjects) {
-			assertThat((Class) inObj.getClass(), equalTo((Class) eachDiffObject.getClass() ));
-			assertThat( "Different object should not == source", eachDiffObject, not( sameInstance(inObj) ));
-			assertThat( "Different object should not equal the Source/Copy object", inObj, not( equalTo(eachDiffObject) ));
-			assertThat( "Source.hashCode() should (probably) not equal the Different.hashCode()", inObj.hashCode(), not( equalTo( eachDiffObject.hashCode() ))); // (AGR) ;-)
-		}
-	}
+        final Set<Object> diffObjects = new HashSet<Object>();
+        diffObjects.addAll( Arrays.asList(inDifferentObjects) );
+        assertThat( "Duplicate Different objects are present: counts differ", inDifferentObjects.length, is(diffObjects.size()));
 
-	private static class CompletelyDifferentClass {
-		// NOOP
-	}
+        for ( Object eachDiffObject : inDifferentObjects) {
+            assertThat((Class) inObj.getClass(), equalTo((Class) eachDiffObject.getClass() ));
+            assertThat( "Different object should not == source", eachDiffObject, not( sameInstance(inObj) ));
+            assertThat( "Different object should not equal the Source/Copy object", inObj, not( equalTo(eachDiffObject) ));
+            assertThat( "Source.hashCode() should (probably) not equal the Different.hashCode()", inObj.hashCode(), not( equalTo( eachDiffObject.hashCode() ))); // (AGR) ;-)
+        }
+    }
+
+    private static class CompletelyDifferentClass {
+        // NOOP
+    }
 }
